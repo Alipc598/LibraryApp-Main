0f13d8d7522b0ccab98b44997cb52d8403f497b6
Alipc598 <117017807+Alipc598@users.noreply.github.com>
2024-05-27 20:35:35 +0200

    Add detailed logging for debugging

--- backend/server.js
+++ backend/server.js
@@ -134,6 +134,11 @@ app.post('/admin', async (req, res) => {
   }
 });
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 // Serve the frontend HTML file for all unknown routes
 app.get('*', (req, res) => {
   res.sendFile(join(__dirname, '..', 'frontend', 'index.html'));
